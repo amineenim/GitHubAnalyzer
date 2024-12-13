87253910bedf5794477d1c393248f8537ded47ec
Amine MAOURID <aminemaourid1995@gmail.com>
2024-12-13 02:13:57 +0100

    initializing the dev environment with services for both backend and frontend

---
+++ backend/app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/api/health', methods=["GET"])
+def health_check():
+    return jsonify({"status" : "healthy"}), 200
+
+if __name__=="__main__":
+    app.run(debug=True)
