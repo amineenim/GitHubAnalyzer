3c8c0ba67bd77dae1a449e8da12f8cf827a3b44b
Amine MAOURID <aminemaourid1995@gmail.com>
2024-12-23 18:26:43 +0100

    defining the styles for the homepage to handle both dark and light themes, with a toggle button to switch the mode

--- frontend/myapp/src/components/Home.js
+++ frontend/myapp/src/components/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Home.css';
 import Input from './Input';
+import {FaSun, FaMoon} from 'react-icons/fa';
 
 const Home = () => {
+    const [theme, setTheme] = useState('dark');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+        document.documentElement.setAttribute('data-theme', theme === 'dark' ? 'light' : 'dark');
+    };
+
     return(
         <div className="home">
             <header>
                 <h1>Github Profile Analyzer</h1>
                 <p>Interested in insights and Activity of a profile on Github, this is your right place</p>
+                <div className="theme-toggle" onClick={toggleTheme} >
+                    {theme === 'dark' ? <FaSun size={20} /> : <FaMoon size={20} />}
+                </div>
             </header>
             <main>
                 <Input />
